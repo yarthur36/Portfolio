583e4b207ee26e1a908993d29622fa6416b9cabd
Arthur Chevalier <88406865+yarthur36@users.noreply.github.com>
2023-07-14 18:22:26 +0000

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://yarthur36.github.io'
+    site: 'https://yarthur36.github.io',
+    base: 'tree/master/dist'
 });
