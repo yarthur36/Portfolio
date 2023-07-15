0751250dce53f7a9d8463b28fbf471666b06b585
Arthur Chevalier <88406865+yarthur36@users.noreply.github.com>
2023-07-15 15:55:20 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://yarthur36.github.io/portfolio',
+    site: 'https://yarthur36.github.io',
+    base : '/portfolio'
 });
